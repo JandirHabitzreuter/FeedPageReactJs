b95e72182a1ec793c7546577e403f7fa53a75bad
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-01-12 20:32:23 -0300

    feat: Including header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import igniteLogo from '../assets/Ignite_logo.svg'
+
 export function Header(){
     return (
         <header className={styles.header}>
-            <strong>
-                Ignite Feed - IF
-            </strong>
+            <img src={igniteLogo}
+                 alt="Logo" 
+             />
         </header>        
     )
 }
