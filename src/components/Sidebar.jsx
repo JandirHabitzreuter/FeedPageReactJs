5e6d6eb72fff5ce4c0dca1c9a98722f1d3d5882f
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-01-12 21:16:19 -0300

    feat: Including sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import style from './Sidebar.module.css';
+
+export function Sidebar(){
+    return (
+        <aside className={style.sidebar}>
+            <img className={style.cover}
+                src="https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
+
+            <div className={style.profile}>
+                <strong> Jandir Habitzreuter </strong>   
+                <span> Web Develop</span>
+            </div>
+
+            <footer>
+                <a href='#'>
+                    Edit
+                </a>    
+            </footer>    
+
+        </aside>
+    );
+}
