f75cf3213db7b769040d94700c2d99244787c972
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-01-16 20:48:57 -0300

    refactor: creating avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import style from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 
 export function Sidebar(){
@@ -9,9 +10,8 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
 
             <div className={style.profile}>
-                <img className={style.avatar} 
-                     src='https://avatars.githubusercontent.com/u/28264580?v=4'/>
-
+                <Avatar src='https://avatars.githubusercontent.com/u/28264580?v=4'/>
+                
                 <strong> Jandir Habitzreuter </strong>   
                 <span> Web Develop</span>
             </div>
